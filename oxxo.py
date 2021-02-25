211399beb8de6b8d066a795fee773cd12787ea9f
Alexandroskw <filipbilly.120@gmail.com>
2021-02-24 20:11:48 -0600

    Problematica oxxo

--- oxxo.py
+++ oxxo.py
@@ -4,7 +4,8 @@ import random, os
 
 #variables globales
 maxClientes = 4         #maximo de clientes en UNA caja
-Clientes = []         #clientes dados por el usuario 
+#Clientes = []          #clientes dados por el usuario
+atendidos = []          #auxiliar
 
 
 #mutexes a usar
@@ -13,16 +14,29 @@ caja2 = Semaphore(maxClientes)
 tramites = Semaphore(0)                 #Mutex por si se hace algún deposito o pago de servicio
 pago = Semaphore(1)                     #Mutex de pago realizado por el cliente
 
-def numClientes():
-    global Clientes
+def cajas():
     #Se introduce manualmente el número de clientes para ser despachados
-    Clientes = int(input("Introducir cuantos clientes seran despachados>> "))
+    #Clientes = int(input("Introducir cuantos clientes seran despachados>> "))
     caja1.acquire()
-    if (round(Clientes) >= 4):       #si el numero de clientes es mayor o igual al número de clientes se adquieren ambas cajas
-        os.system("clear")
-        print("El numero de clientes es mayor al permitido. Habilitando la caja 2...")
-        caja2.acquire()
-        sleep(3)
-        
+    if ((round(Clientes)) >= maxClientes):       #si el numero de clientes es mayor o igual al número de clientes se adquieren ambas cajas
+        while(True):
+            os.system("clear")
+            print("El numero de clientes es mayor al permitido. Habilitando la caja 2...")
+            caja2.acquire()
+            sleep(3)
+            despachados = atendidos.pop(0)
+            caja = atendidos.pop(0)
+            print("\tLa caja %d ha despachado al cliente %d" % (caja, despachados))
+            caja1.release()
+            caja2.release()
+    else:
+        while(True):
+            os.system("clear")
+            
 
-numClientes()
+
+#def clientes():
+
+Clientes = int(input("Introducir cuantos clientes serán atendidos>> "))
+
+cajas()
