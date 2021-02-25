de53cd9783bdff1bea355b4c7f6ee62b989d50a7
Alexandroskw <filipbilly.120@gmail.com>
2021-02-24 20:31:24 -0600

    Problematica oxxo

--- oxxo.py
+++ oxxo.py
@@ -15,28 +15,41 @@ tramites = Semaphore(0)                 #Mutex por si se hace algún deposito o 
 pago = Semaphore(1)                     #Mutex de pago realizado por el cliente
 
 def cajas():
-    #Se introduce manualmente el número de clientes para ser despachados
-    #Clientes = int(input("Introducir cuantos clientes seran despachados>> "))
     caja1.acquire()
-    if ((round(Clientes)) >= maxClientes):       #si el numero de clientes es mayor o igual al número de clientes se adquieren ambas cajas
+    pago.acquire()
+    #si el numero de clientes es mayor o igual al número de clientes se adquieren ambas cajas
+    if ((round(Clientes)) > maxClientes):
         while(True):
             os.system("clear")
             print("El numero de clientes es mayor al permitido. Habilitando la caja 2...")
             caja2.acquire()
             sleep(3)
+            #cuantos clientes han sido despachados
             despachados = atendidos.pop(0)
             caja = atendidos.pop(0)
             print("\tLa caja %d ha despachado al cliente %d" % (caja, despachados))
+            #liberando las cajas
             caja1.release()
             caja2.release()
+            break
+    elif ((round(Clientes)) == 0):
+        os.system("clear")
+        print("No puede no haber ningún cliente")
     else:
         while(True):
             os.system("clear")
-            
+            despachados = atendidos.pop(0)
+            caja = atendidos.pop(0)
+            print("\La caja ha atendido al cliente %d", despachados)
+            break
+
+    caja1.release()
+    pago.release()
+
+def clientes():
 
 
-#def clientes():
-
+os.system("clear")
 Clientes = int(input("Introducir cuantos clientes serán atendidos>> "))
 
 cajas()
