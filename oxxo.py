0db244f9a9a055edbf26173e982fa66c9d078394
Alexandroskw <filipbilly.120@gmail.com>
2021-02-24 19:08:03 -0600

    Problematica oxxo

--- oxxo.py
+++ oxxo.py
@@ -1,24 +1,18 @@
 from threading import Semaphore
 from time import sleep
+import random
 
 #variables globales
 maxClientes = 4         #maximo de clientes en UNA caja
-Clientes = []           #clientes despachados
+Clientes = []           #clientes dados por el usuario 
 
-#Mutexes a usar
-clientes = Semaphore(maxClientes)   #Mutex para llegada de clientes
-caja1 = Semaphore(maxClientes)      #Multiplex para cada caja
-caja2 = Semaphore(0)                #"      "       "       "
-espera = Semaphore(0)               #Mutex de espera por si hay algún deposito o pago de servicio
-exceso = Semaphore(1)               #Mutex de para determinar si hay exceso o no en una caja
 
-#Función de las cajas
-def cajas():
+#mutexes a usar
+caja1 = Semaphore(maxClientes)          #Un mutex de la caja 1, se usa el máximo de clientes
+caja2 = Semaphore(maxClientes)
+tramites = Semaphore(0)                 #Mutex por si se hace algún deposito o pago de servicio
+pago = Semaphore(1)                     #Mutex de pago realizado por el cliente
+
+def numClientes():
     global Clientes
-    while(True):
-        caja1.acquire()             #Las cajas también se inician
-        caja2.acquire()             #(La caja 2 queda iniciada por cualquier cosa)
-        clientes.acquire()          #Adquiriendo clientes
-        out = Clientes.pop(0)       #vaciando los clientes
-
-        print("\tSe ha despachado al cliente: %d", out)
+    Clientes = input("Introducir cuantos clientes seran despachados")
