656f0d27318e9e6f88c276b65b54dff829155d9a
Alexandroskw <filipbilly.120@gmail.com>
2021-02-24 19:18:49 -0600

    Problematica oxxo

--- oxxo.py
+++ oxxo.py
@@ -15,7 +15,10 @@ pago = Semaphore(1)                     #Mutex de pago realizado por el cliente
 
 def numClientes():
     global Clientes, maxClientes
+    #Se introduce manualmente el número de clientes para ser despachados
     Clientes = input("Introducir cuantos clientes seran despachados>> ")
-    if (Clientes >= maxClientes):
-        caja1.acquire()
+    caja1.acquire()
+    if (Clientes >= maxClientes):       #si el numero de clientes es mayor o igual al número de clientes se adquieren ambas cajas
+        print("\t El numeo de clientes es grande. Habilitando la caja 2")
         caja2.acquire()
+    
