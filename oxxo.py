781d44d01489a4c96693f49cac3295a289b7f0bd
Alexandroskw <filipbilly.120@gmail.com>
2021-02-24 19:44:29 -0600

    Problematica oxxo

--- oxxo.py
+++ oxxo.py
@@ -1,10 +1,10 @@
 from threading import Semaphore
 from time import sleep
-import random
+import random, os
 
 #variables globales
 maxClientes = 4         #maximo de clientes en UNA caja
-Clientes = []           #clientes dados por el usuario 
+Clientes = []         #clientes dados por el usuario 
 
 
 #mutexes a usar
@@ -14,11 +14,15 @@ tramites = Semaphore(0)                 #Mutex por si se hace algún deposito o 
 pago = Semaphore(1)                     #Mutex de pago realizado por el cliente
 
 def numClientes():
-    global Clientes, maxClientes
+    global Clientes
     #Se introduce manualmente el número de clientes para ser despachados
-    Clientes = input("Introducir cuantos clientes seran despachados>> ")
+    Clientes = int(input("Introducir cuantos clientes seran despachados>> "))
     caja1.acquire()
-    if (Clientes >= maxClientes):       #si el numero de clientes es mayor o igual al número de clientes se adquieren ambas cajas
-        print("\t El numeo de clientes es mayor al permitido. Habilitando la caja 2")
+    if (round(Clientes) >= 4):       #si el numero de clientes es mayor o igual al número de clientes se adquieren ambas cajas
+        os.system("clear")
+        print("El numero de clientes es mayor al permitido. Habilitando la caja 2...")
         caja2.acquire()
-    
+        sleep(3)
+        
+
+numClientes()
