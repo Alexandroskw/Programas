206a41fdcc88bc8e163754b89e03a9b5739379a6
Alexandroskw <filipbilly.120@gmail.com>
2021-02-25 19:09:59 -0600

    Problematica oxxo

--- oxxo.py
+++ oxxo.py
@@ -46,23 +46,4 @@ def cajas():
 
     caja1.release()
     pago.release()
-
-def clientes(despachados):
-   pago.acquire()               #procediendo al pago
-   tramite = randfloat(tiempo, 120)         #haciendo una varible aleatoria
-   print("El cliente %d ha realizado el pago", despachados)
-   if(tramite >= tiempo):
-       while(True):
-           tramites.acquire()
-           print("El cliente %d ha iniciado un tramite", despachados)
-           atendidos.append(despachados)
-           tramites.release()
-           sleep(2)
-
-os.system("clear")
-Clientes = int(input("Introducir cuantos clientes serán atendidos>> "))
-
-Thread(target = cajas).start()
-
-for i in range(1, Clientes+1):
-    Thread(target = clientes, args = [i]).start()
+    
